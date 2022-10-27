d8b2f63ffdd051f172450e1a9a67338089e82ec4
Paweł Stołowski <stolowski@gmail.com>
2022-10-27 11:51:33 +0200

    Init.

---
+++ src/plugin.cpp
@@ -0,0 +1,34 @@
+#include <XPLMDefs.h>
+#include <XPLMPlugin.h>
+//#include <XPLMProcessing.h>
+//#include <XPLMUtilities.h>
+
+float flighloopCb(float /*inElapsedSinceLastCall*/, float /*inElapsedTimeSinceLastFlightLoop*/, int /*inCounter*/, void * /*inRefcon*/)
+{
+    return 0.1f;
+}
+
+PLUGIN_API int XPluginStart(char *outName, char *outSig, char *outDesc)
+{
+    strcpy(outName, "A plugin");
+    strcpy(outSig, "com.plugin.a");
+    strcpy(outDesc, "A X-Plane plugin");
+    return 1;
+}
+
+PLUGIN_API void XPluginStop(void) {}
+
+PLUGIN_API void XPluginDisable(void)
+{
+    XPLMUnregisterFlightLoopCallback(flighloopCb, NULL);
+}
+
+PLUGIN_API int XPluginEnable(void)
+{
+    XPLMRegisterFlightLoopCallback(flighloopCb, 1, nullptr);
+    return 1;
+}
+
+PLUGIN_API void XPluginReceiveMessage(XPLMPluginID /*inFrom*/, int /*inMsg*/, void * /*inParam*/)
+{
+}
