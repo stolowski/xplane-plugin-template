78c30f94bc9ebf0fd59572e1758ebfc12842413a
Paweł Stołowski <stolowski@gmail.com>
2022-10-31 16:21:00 +0100

    Use XPMMeasureString API

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,11 +1,24 @@
 #include <XPLMDefs.h>
 #include <XPLMPlugin.h>
 #include <XPLMProcessing.h>
+#include <XPLMGraphics.h>
+#include <XPLMUtilities.h>
+
 #include <string.h>
+#include <sstream>
 
 float flighloopCb(float /*inElapsedSinceLastCall*/, float /*inElapsedTimeSinceLastFlightLoop*/, int /*inCounter*/, void * /*inRefcon*/)
 {
-    return 0.1f;
+    static const char *text = "Move Checklist Window Down";
+    auto sz = strlen(text);
+
+    auto m = XPLMMeasureString(xplmFont_Proportional, text, sz);
+
+    std::stringstream s;
+    s << "text len = " << m << "\n";
+
+    XPLMDebugString(s.str().c_str());
+    return 1.0f;
 }
 
 PLUGIN_API int XPluginStart(char *outName, char *outSig, char *outDesc)
