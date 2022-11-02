83d108190b54ad93ec04ada381ebc3659b11d196
Paweł Stołowski <stolowski@gmail.com>
2022-11-02 16:23:39 +0100

    Add widgets include

--- scripts/regenerate_sdk_patch.sh
+++ scripts/regenerate_sdk_patch.sh
@@ -13,21 +13,21 @@ cc = meson.get_compiler('c')
 
 if host_machine.system() == 'darwin'
     sdk_dep = declare_dependency(
-        link_args: ['-framework', 'XPLM', '-F', meson.current_source_dir() + '/Libraries/Mac'],
-        include_directories: include_directories(['CHeaders/XPLM'])
+        link_args: ['-framework', 'XPLM', '-framework', 'XPWidgets', '-F', meson.current_source_dir() + '/Libraries/Mac'],
+        include_directories: include_directories(['CHeaders/XPLM', 'CHeaders/Widgets'])
     )
 endif
 
 if host_machine.system() == 'windows'
     sdk_dep = declare_dependency(
-        link_args: ['-LXPLM_64', '-L', meson.current_source_dir() + '/Libraries/Win'],
-        include_directories: include_directories(['CHeaders/XPLM'])
+        link_args: ['-lXPLM_64', 'XPWidgets_64', '-L', meson.current_source_dir() + '/Libraries/Win'],
+        include_directories: include_directories(['CHeaders/XPLM', 'CHeaders/Widgets'])
     )
 endif
 
 if host_machine.system() == 'linux'
     sdk_dep = declare_dependency(
-        include_directories: include_directories(['CHeaders/XPLM']),
+        include_directories: include_directories(['CHeaders/XPLM', 'CHeaders/Widgets']),
         link_args: ['-shared', '-rdynamic', '-nodefaultlibs', '-undefined_warning']
     )
 endif
